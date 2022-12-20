4f74655a9e3cc996114860dd7c8f9f7fc0b74146
DixonJames <39742785+DixonJames@users.noreply.github.com>
2022-12-20 16:56:33 +0000

    compleated minning compleated q3

--- blockchain_app.py
+++ blockchain_app.py
@@ -2,9 +2,11 @@ from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization, hashes
 import qrcode
 import os
+import json
 import uuid
 import hashlib
 import base64
+import time
 from datetime import datetime
 from flask import Flask, render_template_string, request, jsonify, send_file
 
@@ -251,6 +253,7 @@ class Client:
 
     def sendTransaction(self, receiver, inputs: [Item], outputs: [Item]):
         transaction = Transaction(sender=self, receiver=receiver, inputs=inputs, outputs=outputs)
+
         if transaction.check_valid():
             self.network.boadcastTransaction(transaction)
         else:
@@ -271,21 +274,21 @@ class Transaction:
         :param outputs:
         """
         self.id = uuid.uuid4()
-        self.sender = sender
+        self.sender = sender.key_pair.public_key_str
         self.receiver = receiver
         self.inputs = inputs
         self.outputs = outputs
         self.time = datetime.utcnow()
 
-        self.signature_sender = sender.key_pair.sighn(str(self.details()))
+        self.signature = sender.key_pair.sighn(str(self.details()))
 
     def details(self):
         """
         creates a dict of the attributes of the transaction
         :return:
         """
-        return {"id": self.id,
-                "sender": self.sender.key_pair.public_key_str,
+        return {"id": str(self.id),
+                "sender": self.sender,
                 "receiver": self.receiver,
                 "inputs": str([i.string() for i in self.inputs]),
                 "outputs": str([i.string() for i in self.outputs]),
@@ -361,6 +364,18 @@ class Block:
         sha.update(header.encode('utf-8'))
         return sha.hexdigest()
 
+    def details(self):
+        return {"time_stamp": self.time_stamp,
+                "block_height": self.block_height,
+                "transactions": self.transactions,
+                "version": self.version,
+                "id": self.id,
+                "previous_block_hash": self.previous_block_hash,
+                "nonce": self.nonce,
+                "max_transaction": self.max_transaction,
+                "transaction_count": self.transaction_count,
+                "Merkle_root": self.merkle_tree.root}
+
 
 class BlockChain:
     """
@@ -398,11 +413,10 @@ class BlockChain:
                                           inputs=[start_input],
                                           outputs=[start_output])
         # add block to the chain
-        self.addBlock(transactions=[genesis_transaction],
-                      nonce=0,
-                      genesis=True)
+        self.addGenesisBlock(transactions=[genesis_transaction],
+                             nonce=0)
 
-    def addBlock(self, transactions: [Transaction], nonce: int, genesis=False):
+    def addGenesisBlock(self, transactions: [Transaction], nonce: int):
         """
         adds a block to the chain
         :param transactions: transaction list to be included in the block
@@ -411,10 +425,8 @@ class BlockChain:
         :return:
         """
         # change the previous ash depending on if genesis block or not
-        if not genesis:
-            previous_hash = self.blocks[-1].genHash()
-        else:
-            previous_hash = 0
+
+        previous_hash = None
 
         # create the block
         block = Block(transactions=transactions,
@@ -426,8 +438,41 @@ class BlockChain:
         # append the block
         self.blocks.append(block)
 
+    def addBlock(self, block: Block):
+        """
+        adds a block to the chain only if the chain is valid with it
+        :param block:
+        :return: a boolean as to weather or not the block is accepted
+        """
+        self.blocks.append(block)
+        if not self.verify_chain():
+            self.blocks = self.blocks[:-1]
+            return False
+        return True
+
+    def hashProofs(self, current_proof, previous_proof):
+        """
+        takes two proofs for neighboring blocks on the blockchain
+        and creates a hash of them
+        :param current_proof:
+        :param previous_proof:
+        :return:
+        """
+        proof_string = str(current_proof ** 2 - previous_proof ** 2)
+        sha = hashlib.sha256()
+        sha.update(proof_string.encode('utf-8'))
+        return sha.hexdigest()
+
     def verify_chain(self):
+        """
+        goes through the current chain of blocks and checks:
+        1. current stated block hash is the same as the now computed hash
+        2. the stated previous hash is the same as the hash of the previous block in the chain
+        :return: a boolean as to weather or not the chain is accepted
+        """
         for i in range(len(self.blocks) - 1, 0, -1):
+
+            # block hash checking
             current: Block = self.blocks[i]
             previous: Block = self.blocks[i - 1]
 
@@ -435,6 +480,15 @@ class BlockChain:
                 return False
             if current.previous_block_hash != previous.hash:
                 return False
+
+            # nonce checking
+            # hash of previous proof and current proof is valid hash inside hash difficulty region
+            # checks validity of the current nonce mined
+
+            hash_proofs = self.hashProofs(current_proof=current.nonce, previous_proof=previous.nonce)
+            if hash_proofs[:self.difficulty] != "0" * self.difficulty:
+                return False
+
         return True
 
 
@@ -452,57 +506,177 @@ class Node:
 
         self.unprocessed_transaction_memory = []
         self.processed_transactions_memory = []
-        self.candidate_block = None
+
+        # list of transactions received in valid blocks that have not been received individualy (shouldnt happen here)
+        self.processed_transactions_currently_unreceived = []
+
+        self.blocks_mined = []
+        self.other_blocks_accepted = []
+        self.other_blocks_rejected = []
 
     def receive_transactions(self):
         """
         get unprocessed transactions from the network
         """
         all_transactions = self.network.broadcast_transactions
-        unprocessed = [t for t in all_transactions if t not in self.processed_transactions_memory]
+        unprocessed = [t for t in all_transactions if (t not in self.processed_transactions_memory) and (
+                t not in self.processed_transactions_currently_unreceived)]
 
-        self.unprocessed_transaction_memory.append(unprocessed)
+        self.unprocessed_transaction_memory.extend(unprocessed)
         self.unprocessed_transaction_memory.sort(key=lambda x: x.time)
+
+    def processTransactions(self, transactions: [Transaction]):
+        """
+        update the transaciton sets
+        :param transactions:
+        :return:
+        """
+        to_process = set(transactions)
+        processed = set(self.processed_transactions_memory)
+        unprocessed = set(self.unprocessed_transaction_memory)
+
+        unreceived_transactions = [i for i in to_process if i not in unprocessed]
+
+        processed.update(to_process)
+        self.processed_transactions_memory = list(processed)
+
+        unprocessed.difference_update(to_process)
+        self.unprocessed_transaction_memory = list(unprocessed)
+
+        self.processed_transactions_currently_unreceived = self.processed_transactions_currently_unreceived.extend(
+            unreceived_transactions)
+
+    def addBlock(self, block: Block):
+        block_transacitons = block.transactions
+
+        # check that block has no repeited processed transactions
+        if len(set(self.processed_transactions_memory).intersection(set(block_transacitons))) != 0:
+            return False
+
+        # attempt to add the block to the chain
+        agree = self.chain.addBlock(block=block)
+        if agree:
+            print("block accepted")
+            self.other_blocks_accepted.append(Block)
+
+            # update the transactions sets
+            self.processTransactions(block_transacitons)
+
+        else:
+            print("block rejected")
+            self.other_blocks_rejected.append(block)
+            return False
+
+        return True
 
 
 class Miner(Node):
-    def __init__(self, chain):
-        super().__init__(chain=chain)
+    def __init__(self, chain: BlockChain, network: Network):
+        super().__init__(chain=chain, network=network)
 
-        hash = "1"
-        for i in range(self.chain.difficulty):
-            hash += "1"
-        self.start_hash = hash
-
-        self.sucsess_string = ""
-        for i in range(self.chain.difficulty):
-            self.sucsess_string += "0"
+        self.mine_flag = True
+        self.start_hash = "1" * self.chain.difficulty
+        self.success_string = "0" * self.chain.difficulty
 
     def hashStr(self, s):
         sha = hashlib.sha256()
         sha.update(s.encode('utf-8'))
         return sha.hexdigest()
 
+    def getTransactionsToPorcess(self):
+        """
+        :return: list of maximum processes a block can handle
+        """
+        # finds to process
+        to_process = self.unprocessed_transaction_memory[:self.chain.block_length]
+
+        return to_process
+
+    def getPreviousProof(self):
+        """
+        :return: the previous proof braodcast to the nework
+        """
+        if len(self.network.broadcast_blocks) != 0:
+            return self.network.broadcast_blocks[-1].nonce
+        return 0
+
+    def mine(self, attempts: int = None):
+        """
+        mines continuously untill attemts reached
+        :param attempts: number of rounds to attempt to mine for
+        :return:
+        """
+        while self.mine_flag:
+            previous_proof = self.getPreviousProof()
+            self.POW(previous_proof=previous_proof)
+
+            if attempts is not None:
+                attempts -= 1
+                if attempts == 0:
+                    self.mine_flag = False
+
     def POW(self, previous_proof):
+        """
+        performs proof of work.
+        attempts to mine the next block
+        if successful
+            creates, broadcasts and appends block to chain
+        otherwise:
+            validates the successful broadcast block
+        :param previous_proof: the nonce found in the previous block
+        :return:
+        """
         current_blocks_mined = self.network.blocks_minned
 
         current_hash = self.start_hash
         proof = 0
 
-        while (current_hash[:self.chain.difficulty] != self.sucsess_string) and (self.network.blocks_minned == current_blocks_mined):
-            proof+=1
-            current_hash = self.hashStr(str(proof**2 - previous_proof**2))
+        while (current_hash[:self.chain.difficulty] != self.success_string) and (
+                self.network.blocks_minned == current_blocks_mined):
+            proof += 1
+            current_hash = self.chain.hashProofs(current_proof=proof, previous_proof=previous_proof)
 
+        if current_hash[:self.chain.difficulty] == self.success_string and not (
+                self.network.blocks_minned != current_blocks_mined):
+            # we have a winning proof
+            # need to create our new block and broadcast it
+            self.network.blocks_minned += 1
 
-        if current_hash[:self.chain.difficulty] != self.sucsess_string:
-            #we have a winning proof
-            new_block =
+            new_block = Block(transactions=self.getTransactionsToPorcess(),
+                              index=len(self.chain.blocks) + 1,
+                              previous_hash=self.chain.blocks[-1].hash,
+                              nonce=proof,
+                              max_transaction=self.chain.block_length)
 
+            # broadcast the new block along with the new valid proof
+            self.network.broadcast_blocks.append(new_block)
 
-    def MineBlock(self):
-        current_blocks_mined = self.network.blocks_minned
-        while self.network.blocks_minned == current_blocks_mined:
-            self.POW(self.network.)
+            # add the new block to its own chain
+            accepted = self.addBlock(new_block)
+            if not accepted:
+                print("ERROR - self mined block not accepted")
+
+            self.blocks_mined.append(new_block)
+            time.sleep(1)
+
+        else:
+            # we have failed to mine a valid proof in time have have lost the race
+            # need to give up on our own eforts and validate the new block
+
+            # wait for new block to be uploaded
+            time.sleep(1)
+            # get sucsessful block minned form network
+            unvalidated_new_block = self.network.broadcast_blocks[-1]
+
+            # validates the block and adds to own chain if so
+            agree = self.chain.addBlock(unvalidated_new_block)
+            if agree:
+                print("block accepted")
+                self.other_blocks_accepted.append(unvalidated_new_block)
+            else:
+                print("block rejected")
+                self.other_blocks_rejected.append(unvalidated_new_block)
+
 
 def start_interactive_app():
     """
@@ -513,10 +687,24 @@ def start_interactive_app():
     wp.run_debug()
 
 
-if __name__ == '__main__':
-    network = Network()
+def q_3a():
+    bc = BlockChain(previous_chain=None, difficulty=1, block_length=99)
+    genesis = bc.blocks[-1]
 
-    node_a = Node(chain=BlockChain(), network=network)
+    return {"time_stamp": genesis.time_stamp,
+            "transaction_count": genesis.transaction_count,
+            "previous_block_hash": genesis.previous_block_hash,
+            "nonce": genesis.nonce,
+            "genesis_hash": genesis.genHash(),
+            "genesis_transaction": genesis.transactions[0].details(),
+            }
+
+
+def q_3b():
+    network = Network()
+    bc = BlockChain(previous_chain=None, difficulty=1, block_length=99)
+
+    node_a = Miner(chain=bc, network=network)
 
     client_a = Client(name="Alice", network=network)
     client_b = Client(name="Bob", network=network)
@@ -524,8 +712,29 @@ if __name__ == '__main__':
     client_a.balence = 100
     item_to_send = Item(10)
 
-    client_a.sendTransaction(receiver=client_b.key_pair.public_key,
+    client_a.sendTransaction(receiver=client_b.key_pair.public_key_str,
                              inputs=[item_to_send],
                              outputs=[item_to_send])
 
+    # add some more transactions for submit!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
     node_a.receive_transactions()
+
+    node_a.mine(attempts=1)
+
+    transaction_details = {f"t_{i.genHash()}": i.details() for i in node_a.chain.blocks[-1].transactions}
+
+    results = {"valid nonce":node_a.chain.blocks[-1].nonce,
+               "block hash":node_a.chain.blocks[-1].genHash(),
+               "transactions":transaction_details}
+
+    return results
+
+if __name__ == '__main__':
+    print("q_3a")
+    print(q_3a())
+    print()
+
+    print("q_3b")
+    print(q_3b())
+    print()
